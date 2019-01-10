64c9cfcf0500e7ba2e7258beb63cf916d93417e8
zhangzhiqiang <745124335@aa.com>
2019-01-10 13:52:18 +0800

    modify log info

--- logger/zap_logger.go
+++ logger/zap_logger.go
@@ -36,55 +36,6 @@ var (
 
 type Field = zap.Field
 
-type Logger struct {
-	*zap.Logger
-	module string
-}
-
-func GetLogger(module string) Logger {
-	return Logger{
-		Logger: zapLogger,
-		module: module,
-	}
-}
-
-func (l Logger) appendModule(fields []Field) []Field {
-	if len(l.module) != 0 {
-		fields = append(fields, String("module", l.module))
-	}
-	return fields
-}
-
-func (l Logger) Info(msg string, fields ...Field) {
-	defer Sync()
-	Info(msg, l.appendModule(fields)...)
-}
-
-func (l Logger) Debug(msg string, fields ...Field) {
-	defer Sync()
-	Debug(msg, l.appendModule(fields)...)
-}
-
-func (l Logger) Warn(msg string, fields ...Field) {
-	defer Sync()
-	Warn(msg, l.appendModule(fields)...)
-}
-
-func (l Logger) Error(msg string, fields ...Field) {
-	defer Sync()
-	Error(msg, l.appendModule(fields)...)
-}
-
-func (l Logger) Panic(msg string, fields ...Field) {
-	defer Sync()
-	Panic(msg, l.appendModule(fields)...)
-}
-
-func (l Logger) Fatal(msg string, fields ...Field) {
-	defer Sync()
-	Fatal(msg, l.appendModule(fields)...)
-}
-
 func Info(msg string, fields ...Field) {
 	defer Sync()
 	zapLogger.Info(msg, fields...)
