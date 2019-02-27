af4acac1748d4cba430b9a2dec0e98f6039ac507
kaifei <kaifei@bianjie.ai>
2019-02-27 10:38:52 +0800

    update dependency

--- service/handler/types.go
+++ service/handler/types.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"github.com/irisnet/irishub-sync/logger"
 	"github.com/irisnet/irishub-sync/store/document"
 	"sync"
 )
@@ -16,6 +17,13 @@ func GetTxType(docTx document.CommonTx) string {
 type Action = func(tx document.CommonTx, mutex sync.Mutex)
 
 func Handle(docTx document.CommonTx, mutex sync.Mutex, actions []Action) {
+	defer func() {
+		if err := recover(); err != nil {
+			logger.Error("Parse Tx failed", logger.Int64("height", docTx.Height),
+				logger.String("txHash", docTx.TxHash), logger.Any("err", err))
+		}
+	}()
+
 	for _, action := range actions {
 		if docTx.TxHash != "" {
 			action(docTx, mutex)
