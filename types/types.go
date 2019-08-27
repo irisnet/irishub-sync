5691dc43555a70594b6f11caac4bf10896a05f52
黄 伟昌 <weichang@bianjie.ai>
2019-08-27 16:42:08 +0800

    fix getPrecision method can't handle decimal

--- types/types.go
+++ types/types.go
@@ -196,9 +196,19 @@ func ParseCoin(coinStr string) (coin store.Coin) {
 func getPrecision(amount string) string {
 	length := len(amount)
 	if length > 15 {
-		amount = string([]byte(amount)[:15])
-		for i := 1; i <= length-15; i++ {
-			amount += "0"
+		nums := strings.Split(amount, ".")
+		if len(nums) > 2 {
+			return amount
+		}
+
+		if len_num0 := len(nums[0]); len_num0 > 15 {
+			amount = string([]byte(nums[0])[:15])
+			for i := 1; i <= len_num0-15; i++ {
+				amount += "0"
+			}
+		} else {
+			leng_append := 16 - len_num0
+			amount = nums[0] + "." + string([]byte(nums[1])[:leng_append])
 		}
 	}
 	return amount
