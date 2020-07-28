217d3ce44b228ce17be2e96da6483e6aec9a9efa
huangweichang <weichang@bianjie.ai>
2020-07-28 15:48:00 +0800

    close about vote of proposal save and update

--- service/handler/proposal.go
+++ service/handler/proposal.go
@@ -6,9 +6,7 @@ import (
 	"github.com/irisnet/irishub-sync/util/constant"
 	"github.com/irisnet/irishub-sync/util/helper"
 	"strconv"
-	. "github.com/irisnet/irishub-sync/msg"
 	"github.com/irisnet/irishub-sync/types"
-	"encoding/json"
 )
 
 func handleProposal(docTx *document.CommonTx) {
@@ -34,36 +32,36 @@ func handleProposal(docTx *document.CommonTx) {
 			proposal.VotingEndTime = propo.VotingEndTime
 			store.SaveOrUpdate(proposal)
 		}
-	case constant.TxTypeVote:
-		//失败的投票不计入统计
-		if docTx.Status == document.TxStatusFail {
-			return
-		}
-		if proposal, err := document.QueryProposal(docTx.ProposalId); err == nil {
-			msgVote := DocTxMsgVote{}
-			data, _ := json.Marshal(docTx.Msgs[0].Msg)
-			json.Unmarshal(data, &msgVote)
-			vote := document.PVote{
-				Voter:  msgVote.Voter,
-				Option: msgVote.Option,
-				TxHash: docTx.TxHash,
-				Time:   docTx.Time,
-			}
-			var i int
-			var hasVote = false
-			for i = range proposal.Votes {
-				if proposal.Votes[i].Voter == vote.Voter {
-					hasVote = true
-					break
-				}
-			}
-			if hasVote {
-				proposal.Votes[i] = vote
-			} else {
-				proposal.Votes = append(proposal.Votes, vote)
-			}
-			store.SaveOrUpdate(proposal)
-		}
+		//case constant.TxTypeVote:
+		//	//失败的投票不计入统计
+		//	if docTx.Status == document.TxStatusFail {
+		//		return
+		//	}
+		//	if proposal, err := document.QueryProposal(docTx.ProposalId); err == nil {
+		//		msgVote := DocTxMsgVote{}
+		//		data, _ := json.Marshal(docTx.Msgs[0].Msg)
+		//		json.Unmarshal(data, &msgVote)
+		//		vote := document.PVote{
+		//			Voter:  msgVote.Voter,
+		//			Option: msgVote.Option,
+		//			TxHash: docTx.TxHash,
+		//			Time:   docTx.Time,
+		//		}
+		//		var i int
+		//		var hasVote = false
+		//		for i = range proposal.Votes {
+		//			if proposal.Votes[i].Voter == vote.Voter {
+		//				hasVote = true
+		//				break
+		//			}
+		//		}
+		//		if hasVote {
+		//			proposal.Votes[i] = vote
+		//		} else {
+		//			proposal.Votes = append(proposal.Votes, vote)
+		//		}
+		//		store.SaveOrUpdate(proposal)
+		//	}
 	}
 }
 
--- service/task/task_execute_test.go
+++ service/task/task_execute_test.go
@@ -108,7 +108,7 @@ func Test_parseBlock(t *testing.T) {
 			name: "test parse block",
 			args: args{
 				client: client,
-				b:      6328,
+				b:      11086,
 			},
 		},
 	}
