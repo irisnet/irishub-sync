d1af2d878a2318d2c7d510799f4e92d996c3e556
huangweichang <weichang@bianjie.ai>
2020-09-03 11:26:50 +0800

    adjust for requestrand msg

--- msg/random/rand.go
+++ msg/random/rand.go
@@ -1,13 +1,16 @@
 package random
 
 import (
-	types "github.com/irisnet/irishub-sync/types"
+	"github.com/irisnet/irishub-sync/types"
 	"github.com/irisnet/irishub-sync/util/constant"
+	"github.com/irisnet/irishub-sync/store"
 )
 
 type DocTxMsgRequestRand struct {
-	Consumer      string `bson:"consumer"`       // request address
-	BlockInterval uint64 `bson:"block_interval"` // block interval after which the requested random number will be generated
+	Consumer      string      `bson:"consumer"`       // request address
+	BlockInterval uint64      `bson:"block_interval"` // block interval after which the requested random number will be generated
+	Oracle        bool        `bson:"oracle"`
+	ServiceFeeCap store.Coins `bson:"service_fee_cap"`
 }
 
 func (doctx *DocTxMsgRequestRand) Type() string {
@@ -18,4 +21,6 @@ func (doctx *DocTxMsgRequestRand) BuildMsg(txMsg interface{}) {
 	msg := txMsg.(types.MsgRequestRandom)
 	doctx.Consumer = msg.Consumer.String()
 	doctx.BlockInterval = msg.BlockInterval
+	doctx.Oracle = msg.Oracle
+	doctx.ServiceFeeCap = types.ParseCoins(msg.ServiceFeeCap.String())
 }
